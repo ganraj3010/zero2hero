afa78abc6182881e50b07f45e894a901988103c3
ganraj3010 <ganra3010@gmail.com>
2024-03-30 00:58:27 +0530

    Day4

--- zero2hero/force-app/main/default/lwc/conditionalRendring/conditionalRendring.js
+++ zero2hero/force-app/main/default/lwc/conditionalRendring/conditionalRendring.js
@@ -4,6 +4,7 @@ export default class ConditionalRendring extends LightningElement {
     block = false
 
     toggelBlock(){
+        // falsy values: "", null, Undefine, 0, NaN, false
         this.block = !this.block
     }
 }
